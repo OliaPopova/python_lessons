b55cdb4897133eb74af6ff6b49b7cc17adb8d21d
Olga Popova <olia.popova2001@mail.ru>
2023-01-06 18:51:46 +0300

    урок2 задания

--- Lesson2_Task4.py
+++ Lesson2_Task4.py
@@ -1,15 +1,28 @@
-# Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+# Задайте список из N элементов, заполненных числами из промежутка [-N, N]. Найдите произведение элементов на указанных позициях. Позиции хранятся в файле file.txt в одной строке одно число.
+# Реализуйте алгоритм перемешивания списка.
 
-# Пример:
-
-# - 45 -> 101101
-# - 3 -> 11
-# - 2 -> 10
+import random
 
 
-number = int(input("Введите десятичное число для преобразовывания в двоичное:\n"))
-binarynumber = ""
-while number != 0:
-    binarynumber = str(number % 2) + binarynumber
-    number //= 2
-print(f'{number} ->{binarynumber}')
+def write_file(number):
+    with open('file.txt', 'w') as data:
+        for i in range(number):
+            data.write(f"{random.randrange(0, 2*number)}\n")
+
+
+def read_file():
+    with open('file.txt', 'r') as data:
+        indexs = list(map(int, data.readlines()))
+    return indexs
+
+
+# улучшение проргаммы
+n = int(input("Введите число N: "))
+lst_number = [i for i in range(-n, n+1)]
+write_file(n)
+lst_index = read_file()
+prod = 1
+for i in range(len(lst_index)):
+    prod *= lst_number[lst_index[i]]
+print(f"Результат равен = {prod}")
+
